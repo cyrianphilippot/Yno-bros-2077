56f915756bf75288bd8c4db6a1adff16a6b62a79
malorybergezcasalou <malory.bergezcasalou@ynov.com>
2021-04-13 13:01:14 +0200

    page menu

---
+++ projet/YnoBros2077/src/menu/mapChoice.java
@@ -0,0 +1,67 @@
+package menu;
+
+import menu.pageMenu;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class mapChoice {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mapChoice window = new mapChoice();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mapChoice() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenue ");
+		lblNewLabel.setBounds(0, 11, 150, 14);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Quitter");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton.setBounds(345, 238, 89, 23);
+		panel.add(btnNewButton);
+	}
+}
---
+++ projet/YnoBros2077/src/menu/pageMenu.java
@@ -0,0 +1,82 @@
+package menu;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+
+import com.sun.java.swing.plaf.windows.WindowsOptionPaneUI;
+import com.sun.java.swing.plaf.windows.resources.windows;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class pageMenu {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					pageMenu window = new pageMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public pageMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(174, 80, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String userName = textField.getText();
+				
+			}
+		});
+		btnNewButton.setBounds(174, 123, 86, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Quitter");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+	            System.exit(0);
+			}
+		});
+		btnNewButton_1.setBounds(345, 238, 89, 23);
+		panel.add(btnNewButton_1);
+	}
+}
