bb2a1a1a616c764fdcc191983407a6e5acbae92f
malorybergezcasalou <malory.bergezcasalou@ynov.com>
2021-05-11 10:55:11 +0200

    re add launch lvl 1

--- projet/YnoBros2077/src/ynobros2077/game/level1/com/yno/jeu/jeu/Main.java
+++ projet/YnoBros2077/src/ynobros2077/game/level1/com/yno/jeu/jeu/Main.java
@@ -1,5 +1,7 @@
 package ynobros2077.game.level1.com.yno.jeu.jeu;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -22,4 +24,28 @@ public class Main {
 		fenetre.setContentPane(scene); 
 		fenetre.setVisible(true);		
 	}
+	
+	public static void launchLevel1() {			
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					// Creation de la fenetre de l'application
+					JFrame fenetre = new JFrame("Yno-bros-2077");
+					fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					fenetre.setSize(700, 360);
+					fenetre.setLocationRelativeTo(null);
+					fenetre.setResizable(false);
+					fenetre.setAlwaysOnTop(true);
+
+					// Instanciation de l'objet scene
+					scene = new Scene();
+
+					fenetre.setContentPane(scene); 
+					fenetre.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});		
+}
 }
