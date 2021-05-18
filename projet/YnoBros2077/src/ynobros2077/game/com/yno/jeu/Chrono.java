0802a20262435268cb489c790af7d38a13970179
malorybergezcasalou <malory.bergezcasalou@ynov.com>
2021-05-18 12:55:58 +0200

    debug

--- projet/YnoBros2077/src/ynobros2077/game/com/yno/jeu/Chrono.java
+++ projet/YnoBros2077/src/ynobros2077/game/com/yno/jeu/Chrono.java
@@ -5,15 +5,12 @@ public class Chrono implements Runnable{
 	private final int PAUSE = 3; // temps d'attente entre 2 tours de boucle
 	
 	@Override
-	public void run() {
-
-		while(true){ 
-			
-			Main.scene.repaint();
-			try {
-				Thread.sleep(PAUSE);
-			}catch (InterruptedException e) {}
-		}
-	}
-
+    public void run(){        
+        while(true){ // boucle infinie                        
+            // Instruction repetee a chaque tour de boucle            
+            Main.scene.repaint();            
+            try{Thread.sleep(PAUSE);} // on arrete le flux principal pendant le temps egal a PAUSE en ms
+            catch (InterruptedException e){}
+        }        
+    }
 }
